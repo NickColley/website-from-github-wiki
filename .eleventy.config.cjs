c2b4febcf413784fdc49b286ce20598c7a92b865
Nick Colley <nick.colley@hippodigital.co.uk>
2022-10-24 21:17:57 +0100

    Add some base styling

--- .eleventy.config.cjs
+++ .eleventy.config.cjs
@@ -1,61 +1,31 @@
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
-
-const { BASE_HREF } = require("./lib/constants.cjs");
+const {
+  DefaultFrontmatterPlugin,
+} = require("./lib/plugin/default-frontmatter.cjs");
 
 const {
-  TemplatePath: { join, normalize, getExtension },
-} = require("@11ty/eleventy-utils");
-const DefaultFrontmatterPlugin = (eleventyConfig, pluginConfig = {}) => {
-  const { all, ...files } = pluginConfig;
-  const inputDirectory = eleventyConfig?.dir?.input || ".";
-  let frontmatterStore = new Map();
-  let extensionStore = new Map();
-
-  if (all) {
-    Object.keys(all).forEach((key) => {
-      return eleventyConfig.addGlobalData(key, all[key]);
-    });
-  }
-  if (files) {
-    Object.keys(files).forEach((fileName) => {
-      const frontmatter = files[fileName];
-      const filePath = join(inputDirectory, fileName);
-      if (!filePath) {
-        return;
-      }
-      frontmatterStore.set(filePath, frontmatter);
-      const fileExtension = getExtension(filePath);
-      if (!fileExtension) {
-        return;
-      }
-      extensionStore.set(fileExtension);
-    });
-  }
-
-  const fileExtensions = Array.from(extensionStore.keys()).join(", ");
-  if (!fileExtensions) {
-    return;
-  }
-  eleventyConfig.addDataExtension(fileExtensions, {
-    parser: (filePath) => {
-      const normalisedPath = normalize(filePath);
-      const frontmatter = frontmatterStore.get(normalisedPath);
-      if (frontmatter) {
-        return frontmatter;
-      }
-    },
-    read: false, // return file name instead of content
-  });
-};
+  filterBy,
+  slugToTitle,
+  textLength,
+  print,
+} = require("./lib/filters.cjs");
+const constants = require("./lib/constants.cjs");
+const { BASE_HREF } = constants;
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin, {
     baseHref: BASE_HREF,
   });
 
   // Set default frontmatter
   eleventyConfig.addPlugin(DefaultFrontmatterPlugin, {
+    all: {
+      constants,
+      layout: "page.njk",
+      siteTitle: "Website from GitHub Wiki",
+    },
     "Home.md": {
       permalink: "/",
     },
@@ -66,12 +36,22 @@ module.exports = function (eleventyConfig) {
     delims: ["<!---", "--->"],
   });
 
+  // Filters
+  eleventyConfig.addFilter("capitalize", slugToTitle);
+  eleventyConfig.addFilter("filterBy", filterBy);
+  eleventyConfig.addFilter("slugToTitle", slugToTitle);
+  eleventyConfig.addFilter("textLength", textLength);
+  eleventyConfig.addFilter("print", print);
+
   // Ensure our untracked _wiki input can be used as an input.
   eleventyConfig.setUseGitIgnore(false);
 
   return {
     dir: {
+      includes: "../_includes",
       input: "_wiki",
     },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
   };
 };
