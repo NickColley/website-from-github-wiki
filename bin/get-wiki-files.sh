be2a236968afe26525f922e38d5ffb0b3a89c9dc
Nick Colley <nick.colley@hippodigital.co.uk>
2022-10-24 18:17:41 +0100

    Add placeholder functionality

--- bin/get-wiki-files.sh
+++ bin/get-wiki-files.sh
@@ -1,13 +1,20 @@
-if [ $CI ]; then
-    echo "Running in CI, already cloned wiki files as part of GitHub actions..."
-else
-    outputDirectory="_wiki"
-    echo "Cleaning up '${outputDirectory}'..."
-    rm -rf $outputDirectory
+outputDirectory="_wiki"
+if ! [ $CI ]; then
+    if [ -d $outputDirectory ]; then
+        echo "Cleaning up '${outputDirectory}'..."
+        rm -rf $outputDirectory
+    fi
     remoteName=$(git branch --list "$(git branch --show-current)" "--format=%(upstream:remotename)")
     remoteUrl=$(git remote get-url ${remoteName})
     # If there is a git extension remove it
     removedGitExtension=${remoteUrl/\.git/}
     wikiRemoteUrl="${removedGitExtension}.wiki.git"
     git clone $wikiRemoteUrl $outputDirectory
+fi
+
+# If the output directory does not exist setup a placeholder.
+if ! [ -d $outputDirectory ]; then
+    echo "No source files, so setup some placeholder files to use..."
+    mkdir $outputDirectory
+    echo "# Hello, world." > "$outputDirectory/Home.md"
 fi
