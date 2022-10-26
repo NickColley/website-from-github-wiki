6f01c9f05839191a3b9dfdab755ca1ee9e485b88
Nick Colley <2445413+nickcolley@users.noreply.github.com>
2022-10-27 00:04:01 +0100

    Try github actions via script again

--- bin/get-wiki-files.sh
+++ bin/get-wiki-files.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 OUTPUT_DIRECTORY="_wiki"
-if [ $CI ]; then
-    # If the output directory does not exist setup a placeholder.
-    if ! [ -d $OUTPUT_DIRECTORY ]; then
-        echo "No source files, so create a placeholder..."
-        PLACEHOLDER="[Create your first wiki page](https://github.com/$GITHUB_REPOSITORY/wiki)"
-        mkdir $OUTPUT_DIRECTORY
-        echo $PLACEHOLDER > "$OUTPUT_DIRECTORY/Home.md"
-    fi
-else
-    if [ -d $OUTPUT_DIRECTORY ]; then
-        echo "Cleaning up '${OUTPUT_DIRECTORY}'..."
-        rm -rf $OUTPUT_DIRECTORY
-    fi
-    REMOTE_URL=$(./bin/get-remote-url.sh)
-    # If there is a git extension remove it
-    REMOVED_GIT_EXTENSION=${REMOTE_URL/\.git/}
-    WIKI_REMOTE_URL="${REMOVED_GIT_EXTENSION}.wiki.git"
+if [ -d $OUTPUT_DIRECTORY ]; then
+    echo "Cleaning up '${OUTPUT_DIRECTORY}'..."
+    rm -rf $OUTPUT_DIRECTORY
+fi
+REMOTE_URL=$(./bin/get-remote-url.sh)
+# If there is a git extension remove it
+REMOVED_GIT_EXTENSION=${REMOTE_URL/\.git/}
+WIKI_REMOTE_URL="${REMOVED_GIT_EXTENSION}.wiki.git"
+if git ls-remote --exit-code $wikiRemoteUrl &> /dev/null; then
     git clone $WIKI_REMOTE_URL $OUTPUT_DIRECTORY
+else
+    echo "Remote repo '${WIKI_REMOTE_URL}' does not exist."
+fi
+# If the output directory does not exist setup a placeholder.
+if ! [ -d $OUTPUT_DIRECTORY ]; then
+    echo "No source files, so create a placeholder..."
+    PLACEHOLDER="[Create your first wiki page](https://github.com/$GITHUB_REPOSITORY/wiki)"
+    mkdir $OUTPUT_DIRECTORY
+    echo $PLACEHOLDER > "$OUTPUT_DIRECTORY/Home.md"
 fi
